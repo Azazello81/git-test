abfe334437e065dbaefbe326db76cf78bc5102e5
Max Morichkin <legat-sv@bk.ru>
2023-01-05 20:39:51 +0300

    add new js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,4 @@
-document.body.innerHTML='<h2>привет из JS!</h2>'
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.innerHTML='<h2>привет из JS!</h2>'
+    console.log('Privet')
+    })
